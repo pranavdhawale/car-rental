0be86686b323783188c9cdb6ef9474cb9275c403
Aryan Chauhan <aryanrc28@gmail.com>
2025-04-06 19:10:58 +0530

    delete: maintenance and filter service

--- filter_service/src/main/java/com/car_rental/filter_service/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.car_rental.filter_service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- maintenance_service/src/main/java/com/car_rental/maintenance_service/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.car_rental.maintenance_service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
